40c71c8f845ed87a9014553013c23032f8228b5c
Basilio <basilioherimiharisoa@gmail.com>
2025-08-26 19:38:37 +0300

    Code ecrit par BASILIO HERIMIHARISOA-1

--- Models/Tourist.py
+++ Models/Tourist.py
@@ -1,31 +1,5 @@
-from Models.model import Base
-from Models.Group import Group
-
-class Tourist:
-    def __init__(self, id, base: Base):
-        self.numero = id
-        self.base = base
-        self.nom = None
-        self.codeG = None
-        self.group = None
-
-        if self.base.con is not None:
-            self.load_from_db()
-
-    def load_from_db(self):
-        query = "SELECT Nom, CodeG FROM touriste WHERE Numero = %s"
-        self.base.cur.execute(query, (self.numero,))
-        result = self.base.cur.fetchone()
-        if result:
-            self.nom = result["Nom"]
-            self.codeG = result["CodeG"]
-            self.group = Group(self.codeG, self.base)
-            self.group.add_member(self)
-
-    def get_nom(self):
-        return self.nom
-
-
-
-
-
+BBBBB     AAAAA   SSSSS  IIIII  L      IIIII   OOOOO        M     M   -   CCCCC   OOOOO  DDDDD   EEEEE
+B    B   A     A  S        I    L        I    O     O       MM   MM       C      O     O D    D  E
+BBBBB    AAAAAAA   SSSS    I    L        I    O     O       M M M M   -   C      O     O D     D EEEE
+B    B   A     A       S   I    L        I    O     O       M  M  M       C      O     O D    D  E
+BBBBB    A     A  SSSSS  IIIII  LLLLL  IIIII   OOOOO        M     M   -   CCCCC   OOOOO  DDDDD   EEEEE
