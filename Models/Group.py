18150b5727b036ef01e0e5c2e4499f7b4d715a56
Basilio <basilioherimiharisoa@gmail.com>
2025-08-27 09:53:58 +0300

    GROUPE mr FANO

--- Models/Group.py
+++ Models/Group.py
@@ -1,5 +1,45 @@
-BBBBB     AAAAA   SSSSS  IIIII  L      IIIII   OOOOO        M     M   -   CCCCC   OOOOO  DDDDD   EEEEE
-B    B   A     A  S        I    L        I    O     O       MM   MM       C      O     O D    D  E
-BBBBB    AAAAAAA   SSSS    I    L        I    O     O       M M M M   -   C      O     O D     D EEEE
-B    B   A     A       S   I    L        I    O     O       M  M  M       C      O     O D    D  E
-BBBBB    A     A  SSSSS  IIIII  LLLLL  IIIII   OOOOO        M     M   -   CCCCC   OOOOO  DDDDD   EEEEE
+from Models.model import Base
+class Group:
+    __base = Base() #class
+    def __init__(self, code):
+        self.base = Base() #objet
+        self.code = code
+        self.nomG = None
+
+        self.base.cur.execute("SELECT NomG FROM groupe WHERE code = %s", (self.code,))
+        dico = self.base.cur.fetchone()
+
+        if dico:
+            self.nomG = dico["NomG"]
+
+    def __str__(self):
+        chaine = f"Code: {self.code}\n Nom: {self.nomG}\n"
+        return chaine
+
+    @classmethod
+    def get_all(cls):
+        cls.__base.cur.execute("SELECT * FROM groupe")
+        return cls.__base.cur.fetchall()
+
+    @classmethod
+    def get_by_id(cls, id):
+            cls.__base.cur.execute("SELECT * FROM groupe WHERE code = %s", (id,))
+            return cls.__base.cur.fetchone()
+
+    @classmethod
+    def insert(cls, nom):
+        cls.__base.cur.execute("INSERT INTO groupe VALUES (NULL, %s)", (nom,))
+        cls.__base.con.commit()
+
+    @classmethod
+    def update(cls, id, nom):
+        cls.__base.cur.execute("UPDATE groupe SET nomG = %s WHERE code = %s", (nom, id))
+        cls.__base.con.commit()
+
+    @classmethod
+    def delete(cls, id):
+        cls.__base.cur.execute("DELETE FROM groupe WHERE code = %s", (id,))
+        cls.__base.con.commit()
+
+
+
